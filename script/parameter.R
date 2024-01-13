4980428d2009dc657f3c07c6076888153d0b4ada
yossawat-suw <point0907@gmail.com>
2024-01-13 22:48:36 +0000

    update code

--- script/parameter.R
+++ script/parameter.R
@@ -1,30 +1,50 @@
 
-# Choose group to import either control or gbm
-radiation <- c("control", "radiated")
-runs <- c("run1", "run2")
-
-# Parameter
-# cell input to be analyse
+## common
 
 object <- "all"
 
-sigs <- c("6metamodules", "4metamodules_new_50", "3metamodules_new_50", "4metamodules_new_50_mes")
 
-sig <- sigs[2]
+
+#marker-based
+sigs <- c( "4metamodules_new_50_mes",
+           "2metamodules_NPC_OPC", 
+           "3metamodules_new_50", 
+           "4metamodules_new_50",
+           "npcopc_ac_mes",
+           "npcopc_acmes")
+
 
 # For reference based
-merges <- c("6metamodules", "4_merge_metamodules", "4_merge_metamodules_3celltypes", "4_merge_metamodules_mes")
+merges <- c("4_merge_metamodules_mes",
+            "2_merge_metamodules", 
+            "4_merge_metamodules_3celltypes",
+            "4_merge_metamodules",
+            "npcopc_ac_mes",
+            "npcopc_acmes")
 
-merge <- merges[2]
-# pick which celltype to be analyse
 
-# all_celltypes <- c("AClike", "MESlike", "NPClike", "OPClike")
-# chosen_celltypes <- all_celltypes[c(2)]
-#annotation
+all_cellstates <- c("MESlike",
+                   "AClike", 
+                   "NPClike", 
+                   "OPClike",
+                   "NPC_OPClike",
+                   "AC_MESlike")
 
-# For tool to be analized in annotation
-anno.picks <- c(by_type = 1, by_consistency = 2, only_diverge = 3, marker_based = 4)
-anno.pick <- anno.picks[3]
+chosing <- list(c(1),c(2,5),c(2,3,4),c(1,2,3,4),c(1,2,5),c(5,6))
+
+
+
+
+## common
+
+object <- "all"
+
+run_eachs <- c(TRUE,FALSE)
+runs <- c("each","whole")
+
+
+## consensus
+unknowns <- c("","_no_unknown")
 
 
 
